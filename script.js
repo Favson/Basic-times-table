2ae6ebd3eee53d14cb58e4bc0d9b182ffd3b99e8
Adebisi <adebisimomoluwa@gmail.com>
2023-12-26 21:55:07 +0100

    times-table updated file

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-// var columns =parseInt(document.getElementById("columns").value)
-// var row = parseInt(document.getElementById("rows").value)
+var columns =parseInt(document.getElementById("columns").value)
+var row = parseInt(document.getElementById("rows").value)
 
-// function makeATable() {
-//     if("add"){
-//         for(x=1; x<=columns; x++){
-//             for(y=1; y<=row; y++){
-//                 displayResult.innerHTML += `<h3 align="center" style="color:greenyellow;"> ${x} + ${y} = ${x+y}</h3>`
-//             }
-//         }
-//     }else if("sub"){
-//         for(x=1; x<=columns; x++){
-//             for(y=1; y<=row; y++){
-//                 displayResult.innerHTML += `<h3 align="center" style="color:greenyellow;"> ${x} - ${y} = ${x-y}</h3>`
-//             }
-//         }
-//     }
-// }
+function makeATable(val){
+    var sign = selection.value
+    var tab = "<table>"
+    tab += "<tr>"
+    for (var i = 1; i<=columns; i++){
+        tab += "<td>"
+        for (var j=1; j<=row;j++){
+            var calcValue = (eval(i + sign + j))
+    
+            if (calcValue % 1 !==0){
+                var calcValue = parseFloat(eval(i + sign + j)).toFixed(2)
+            }
+            tab+= i + sign + j + "=" + calcValue +"<br>" + "<br>" ;
+        }
+            
+    }
+    tab+= "</table>"
+    dispDiv.innerHTML = tab
+}
+
+
 
