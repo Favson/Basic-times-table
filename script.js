d17d4ae6936c4ca0ccb88ea287f2dd50de9a39cc
Adebisi <adebisimomoluwa@gmail.com>
2023-12-26 19:23:29 +0100

    times-table first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// var columns =parseInt(document.getElementById("columns").value)
+// var row = parseInt(document.getElementById("rows").value)
+
+// function makeATable() {
+//     if("add"){
+//         for(x=1; x<=columns; x++){
+//             for(y=1; y<=row; y++){
+//                 displayResult.innerHTML += `<h3 align="center" style="color:greenyellow;"> ${x} + ${y} = ${x+y}</h3>`
+//             }
+//         }
+//     }else if("sub"){
+//         for(x=1; x<=columns; x++){
+//             for(y=1; y<=row; y++){
+//                 displayResult.innerHTML += `<h3 align="center" style="color:greenyellow;"> ${x} - ${y} = ${x-y}</h3>`
+//             }
+//         }
+//     }
+// }
+
